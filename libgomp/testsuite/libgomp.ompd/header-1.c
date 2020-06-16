fb99ef60aef2ce223abb6beea73d01fae02019ed
y2s1982 <y2s1982@gmail.com>
2020-06-16 17:19:24 -0400

    feat: added tests for omp-tools.h header compilation

---
+++ libgomp/testsuite/libgomp.ompd/header-1.c
@@ -0,0 +1,6 @@
+/*  Test that the omp-tools.h will compile successfully.  */
+
+/* { dg-do run } */
+#include "omp-tools.h"
+
+int main(void){ return 0; }
---
+++ libgomp/testsuite/libgomp.ompd/header-order-1.c
@@ -0,0 +1,7 @@
+/*  Test that the omp-tools.h will compile successfully with omp.h .  */
+
+/* { dg-do run } */
+#include "omp-tools.h"
+#include "omp.h"
+
+int main(void){ return 0; }
