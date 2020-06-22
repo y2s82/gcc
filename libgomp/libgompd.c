0f93262b33109386a3f1491e347305e757ac344f
y2s82 <1997371+y2s82@users.noreply.github.com>
2020-06-22 18:12:02 -0400

    feat: updated ompd_init(); updated SONAME_SUFFIX macros

--- libgomp/libgompd.c
+++ libgomp/libgompd.c
@@ -1,13 +1,26 @@
 #include "omp-tools.h"
 #include "libgompd.h"
+#include "plugin-suffix.h"
 
 void ompd_init()
 {
     static int ompd_initialized = 0;
 
+    const char *prefix = "libgompd.";
+    const char *suffix = SONAME_SUFFIX (1);
+
     if (ompd_initialized)
         return;
 
+    size_t prefix_len, suffix_len;
+    prefix_len = strlen(prefix);
+    suffix_len = strlen(suffix);
+
+    ompd_name = (char *) malloc (prefix_len + suffix_len + 1);
+    memcpy (ompd_name, prefix, prefix_len);
+    memcpy (ompd_name + prefix_len, suffix, suffix_len + 1);
+
+    ompd_dell_locations = { ompd_name, NULL };
+
     ompd_initialized = 1;
-    ompd_dell_locations = { "libgompd" SONAME_SUFFIX (1), NULL };
 }
