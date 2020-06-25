298e8a3112a5f5b5baf7474aba9c529b2ecb7548
y2s82 <1997371+y2s82@users.noreply.github.com>
2020-06-24 20:51:05 -0400

    style(ompd-lib.c): fix styling

--- libgomp/ompd-lib.c
+++ libgomp/ompd-lib.c
@@ -35,21 +35,21 @@
 const char *ompd_dll_locations[2] = { "libgompd" SONAME_SUFFIX (1), NULL };
 
 ompd_rc_t 
-ompd_get_api_version ( ompd_word_t *version )
+ompd_get_api_version (ompd_word_t *version)
 {
   *version = OMPD_VERSION;
   return ompd_rc_ok;
 }
 
 ompd_rc_t
-ompd_get_version_string ( const char **string )
+ompd_get_version_string (const char **string)
 {
   *string = "GNU OpenMP Runtime implementing OpenMP 5.0" ompd_str2(OMPD_VERSION);
   return ompd_rc_ok;
 }
 
 ompd_rc_t 
-ompd_initialize ( ompd_word_t api_version, const ompd_callbacks_t *callbacks )
+ompd_initialize (ompd_word_t api_version, const ompd_callbacks_t *callbacks)
 {
   /* initialized flag */
   static int ompd_initialized = 0;
